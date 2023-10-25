bd7b2cd9915d3acb4de8e9d4fb243be8177a891b
r-tsurusaki <ryo-tsurusaki@ginga.info>
2023-10-26 00:52:41 +0900

    init test.

---
+++ src/test/java/jp/tsurusaki/application/controller/OpenAiChatControllerTest.java
@@ -0,0 +1,90 @@
+package jp.tsurusaki.application.controller;
+
+import jp.tsurusaki.domain.service.OpenAiChatService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.anyString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OpenAiChatControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    OpenAiChatService openAiChatService;
+
+    @DisplayName("/openAiChat")
+    @Nested
+    class OpenAiChatTest {
+
+        private final String PATH = "/openAiChat";
+
+        @DisplayName("200（正常系）")
+        @Test
+        void ok() throws Exception {
+
+            String messageText = "リクエスト";
+            String expected = "レスポンス";
+
+            when(openAiChatService.openAiChat(messageText)).thenReturn(expected);
+
+            String path = this.PATH.concat("?message_text=").concat(messageText);
+
+            MvcResult result = mockMvc.perform(
+                    MockMvcRequestBuilders
+                            .get(path))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                    .andReturn();
+
+            String actual = result.getResponse().getContentAsString();
+            assertThat(actual).isEqualTo(expected);
+            verify(openAiChatService, times(1)).openAiChat(messageText);
+        }
+
+        @DisplayName("500（異常系）")
+        @Test
+        void internalServerError() throws Exception {
+
+            String messageText = "リクエスト";
+
+            doThrow(new RuntimeException("エラー"))
+                    .when(openAiChatService).openAiChat(anyString());
+
+            String path = this.PATH.concat("?message_text=").concat(messageText);
+
+            mockMvc.perform(
+                            MockMvcRequestBuilders
+                                    .get(path))
+                    .andExpect(status().isInternalServerError())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andReturn();
+
+            assertThatThrownBy(() -> {
+                throw new RuntimeException("エラー");
+            });
+
+            verify(openAiChatService, times(1)).openAiChat(anyString());
+        }
+    }
+}
---
+++ src/test/java/jp/tsurusaki/domain/service/OpenAiChatServiceTest.java
@@ -0,0 +1,80 @@
+package jp.tsurusaki.domain.service;
+
+import jp.tsurusaki.domain.model.OpenAiChatData;
+import jp.tsurusaki.domain.repository.OpenAIChatRepository;
+import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.fail;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class OpenAiChatServiceTest {
+
+    private AutoCloseable closeable;
+
+    @InjectMocks
+    private OpenAiChatService openAiChatService;
+
+    @Mock
+    private OpenAIChatRepository openAIChatRepository;
+
+    @BeforeEach
+    void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @DisplayName("openAiChat")
+    @Nested
+    class OpenAiChat {
+
+        @DisplayName("正常系")
+        @Test
+        void success() {
+
+            String messageText = "リクエスト";
+
+            OpenAiChatData openAiChatData = OpenAiChatData.builder()
+                    .content("レスポンス")
+                    .build();
+            String expected = openAiChatData.getContent();
+
+            doReturn(openAiChatData).when(openAIChatRepository).openAiChat(messageText);
+
+            String actual = openAiChatService.openAiChat(messageText);
+
+            assertThat(actual).isEqualTo(expected);
+            verify(openAIChatRepository, times(1)).openAiChat(messageText);
+        }
+
+        @DisplayName("異常系")
+        @Test
+        void failure() {
+
+            String messageText = "リクエスト";
+            String errorMessage = "エラー";
+
+            doThrow(new RuntimeException(errorMessage)).when(openAIChatRepository).openAiChat(messageText);
+
+            try {
+                openAiChatService.openAiChat(messageText);
+                fail("No exception Occurred.");
+
+            } catch (Exception exception) {
+
+                assertThat(exception.getMessage()).isEqualTo(errorMessage);
+                verify(openAIChatRepository, times(1)).openAiChat(messageText);
+            }
+        }
+    }
+}
